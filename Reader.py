ebfa89ffd7bcbf52700ff28f570962514e604d1b
Celso J. P. Junior <cpereirajr442@gmail.com>
2020-06-01 22:46:00 -0300

    adicionado salvamento de configuração de voz. arquivo Reader.py

--- Reader.py
+++ Reader.py
@@ -1,4 +1,5 @@
 import imports.speacker_interfice as tts
+import shelve 
 from tkinter import *
 from tkinter.ttk import Combobox
 from tkinter import scrolledtext
@@ -11,8 +12,9 @@ class WindowProperties:
    
     def __init__(self, window):
     #var
+        config = self._login()
         self.engine = tts.Speaker()
-        list_voices = self.engine.all_voice()
+        self.list_voices = self.engine.all_voice()
     #windows_top
         self.Frame_top = Frame(window, bg=bg_color, width=590, height=60,highlightbackground=main_color, highlightthickness=1)
         self.frame_top_voices = Frame(self.Frame_top, bg=bg_color)
@@ -22,13 +24,14 @@ class WindowProperties:
     #windos top Widget
         self.label_voice = Label(self.frame_top_voices, text='Selecione as vozes', font=main_font, bg=bg_color, fg = main_color)
         self.VoicesCombox = Combobox(self.frame_top_voices, font = main_font, background=bg_color, foreground=main_color, width = 45)
-        self.VoicesCombox['values'] = [i for i in list_voices.keys()]
+        self.VoicesCombox['values'] = [i for i in self.list_voices.keys()]
+        self.VoicesCombox.current(config['voice'])
         
         self.label_volume_text = Label(self.frame_vol, text='Volume', font = main_font, bg= bg_color, fg = main_color)
         self.label_rate_text = Label(self.frame_rate, text= 'Velocidade', font = main_font, bg = bg_color, fg = main_color)
         
-        self.label_indice1 = Label(self.frame_vol, font = main_font, bg = 'white', fg = main_color, text = 0, width =6)
-        self.label_indice2 = Label(self.frame_rate, font = main_font, bg = 'white', fg = main_color, text = 0, width = 6)
+        self.label_indice1 = Label(self.frame_vol, font = main_font, bg = 'white', fg = main_color, text = config['vol'], width =6)
+        self.label_indice2 = Label(self.frame_rate, font = main_font, bg = 'white', fg = main_color, text = config['rate'], width = 6)
 
         self.vol_button_plus = Button(self.frame_vol, text = '+', font= main_font, bg = bg_color, fg = main_color, width= 6, command = self.volume_plus )
         self.vol_button_minus = Button(self.frame_vol, text = '-', font= main_font, bg = bg_color, fg= main_color, width= 6, command = self.volume_minus)
@@ -80,8 +83,11 @@ class WindowProperties:
         pass
 
     def ReadText(self):
-        self.engine.speak(self.textbox.get(1.0,END))
-
+       self._save()
+       self.engine.set_voice(self.list_voices[self.VoicesCombox.get()])
+       self.engine.speak(self.textbox.get(1.0,END))
+            
+    #vol define
     def volume_plus(self):
         self.volume_value('+')
     def volume_minus(self):
@@ -92,10 +98,12 @@ class WindowProperties:
         if control == '-' and volume_indice != 0 :
             self.label_indice1['text'] = str(volume_indice - 1)
             self.engine.set_volume(int(self.label_indice1['text']))
+        
         elif control == '+' and volume_indice != 100:
             self.label_indice1['text'] = str(volume_indice + 1)
             self.engine.set_volume(int(self.label_indice1['text']))
 
+    # rate define 
     def rate_plus(self):
         self.rate_value('+')
     def rate_minus(self):
@@ -110,6 +118,32 @@ class WindowProperties:
         elif control == '+' and volume_indice != 300:
             self.label_indice2['text'] = str(volume_indice + 25)
             self.engine.set_rate(int(self.label_indice2['text']))
+
+    def retur_number_by_value(self, voice):
+        numero = 0
+        for i in self.list_voices.keys():
+            if i == self.VoicesCombox.get():
+                return numero
+            numero += 1
+
+    def _save(self):
+        with shelve.open('config') as db:
+            db['voice'] = self.retur_number_by_value(self.VoicesCombox.get())
+            db['vol'] = self.label_indice1['text']
+            db['rate'] = self.label_indice2['text']
+
+    def _login(self):
+        dic = {}
+        try:
+            with shelve.open('config') as db:
+                dic['voice'] = db['voice']
+                dic['vol'] = db['vol']
+                dic['rate'] = db['rate']
+        except:
+            dic['voice'] = 0
+            dic['vol'] = '100'
+            dic['rate'] = '150'
+        return dic
 
 root = Tk()
 root.geometry('600x440+400+150')
